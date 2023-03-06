7120a3fc3565a95a0dee5062bf26f6ea55d27ad8
LikeMattorSomething <75433124+LikeMattorSomething@users.noreply.github.com>
2023-03-06 14:51:15 +0000

    Add files via upload

--- DBViewer.py
+++ DBViewer.py
@@ -23,9 +23,13 @@ st.title('Pumpflow SQL Database Searcher')
 with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
     with conn.cursor() as cursor:
 
-        def SQLSearch_JobAndTest(JobNumber: int = None,TestNumber: int = None):
+        def SQLSearch_JobAndTest():
 
-            ##Makes a connection to the database. Args defined earlier
+
+            JobNumber = st.number_input('Job Number')
+            TestNumber = st.number_input('Test Number')
+
+
             TempList = cursor.execute("SELECT TOP 100 * FROM MainTable WHERE Job_Number = ? AND Test_Number = ?",JobNumber,TestNumber).fetchall()
             st.table(pd.DataFrame.from_records(TempList,columns=["Time","Job_Number","Test_Number","Take_Point","NGH","Efficiency","Absorbed_Power","Flow_Rate","Suction_Pressure","Discharge_Pressure","V1_DEX","V1_DEY","V1_DEZ","V2_NDEX","V2_NDEY","V2_NDEZ","TT1","Out_Torque","RPM_Used","Current","Voltage","Power","Current_P1","Current_P2","Current_P3","Frequency","LT1","PT_Atm"]))
 
@@ -41,6 +45,6 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
         SortMethod = st.radio('Sort by', ['Job and Test Number', 'Date'], horizontal=True)
 
         if SortMethod == 'Job and Test Number':
-            SQLSearch_JobAndTest()
+            SQLSearch_JobAndTest
         elif SortMethod == "Date":
             SQLSearch_Date()
