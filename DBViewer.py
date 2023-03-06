59e7e1b3a2b51d688e12a1ad413c6f3910a6077b
LikeMattorSomething <75433124+LikeMattorSomething@users.noreply.github.com>
2023-03-06 15:33:24 +0000

    Add files via upload

--- DBViewer.py
+++ DBViewer.py
@@ -25,20 +25,20 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
 
         def SQLSearch_JobAndTest():
 
-            SelectTable = st.selectbox('Select a table to display',('MainTable','JobInfo','AddTemperatures','AddAuxPressure','AddAuxFlow','Value_Description'))
+            SelectTable = st.selectbox("Select a table to display",("MainTable","JobInfo","AddTemperatures","AddAuxPressure","AddAuxFlow","Value_Description"))
             JobNumber = st.number_input('Job Number',value=0)
             TestNumber = st.number_input('Test Number',value=0)
 
-            TempList = cursor.execute("SELECT TOP 100 * FROM ? WHERE Job_Number = ? AND Test_Number = ?",(SelectTable,JobNumber,TestNumber)).fetchall()
+            TempList = cursor.execute(f"SELECT TOP 100 * FROM {SelectTable} WHERE Job_Number = ? AND Test_Number = ?",SelectTable,JobNumber,TestNumber).fetchall()
             st.table(pd.DataFrame.from_records(TempList,columns=["Time","Job_Number","Test_Number","Take_Point","NGH","Efficiency","Absorbed_Power","Flow_Rate","Suction_Pressure","Discharge_Pressure","V1_DEX","V1_DEY","V1_DEZ","V2_NDEX","V2_NDEY","V2_NDEZ","TT1","Out_Torque","RPM_Used","Current","Voltage","Power","Current_P1","Current_P2","Current_P3","Frequency","LT1","PT_Atm"]))
 
 
 
         def SQLSearch_Date():
 
-            SelectTable = st.selectbox("Select a table to display",("MainTable","AddTemperatures","AddAuxPressure","AddAuxFlow"),)
+            SelectTable = st.selectbox("Select a table to display",("MainTable","AddTemperatures","AddAuxPressure","AddAuxFlow"))
             Date = st.date_input("Choose a date")
-            TempList = cursor.execute("SELECT TOP 100 * FROM ? WHERE CONVERT(DATETIME, FLOOR(CONVERT(FLOAT, Time))) = ?",(SelectTable,Date)).fetchall()
+            TempList = cursor.execute(f"SELECT TOP 100 * FROM {SelectTable} WHERE CONVERT(DATETIME, FLOOR(CONVERT(FLOAT, Time))) = ?",SelectTable,Date).fetchall()
             st.table(pd.DataFrame.from_records(TempList,columns=["Time","Job_Number","Test_Number","Take_Point","NGH","Efficiency","Absorbed_Power","Flow_Rate","Suction_Pressure","Discharge_Pressure","V1_DEX","V1_DEY","V1_DEZ","V2_NDEX","V2_NDEY","V2_NDEZ","TT1","Out_Torque","RPM_Used","Current","Voltage","Power","Current_P1","Current_P2","Current_P3","Frequency","LT1","PT_Atm"]))
 
 
