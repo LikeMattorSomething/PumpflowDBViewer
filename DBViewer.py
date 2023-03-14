657ccbe29e0846be4cd3c761c225286ffb6fc154
LikeMattorSomething <75433124+LikeMattorSomething@users.noreply.github.com>
2023-03-14 14:34:04 +0000

    Added pages to the tables
    The data that has been selected will only show 100 rows at a time to not slow the site down.
    NOTE: might need to be revisited to improve the efficiency of the database query. Currently gets the whole dataset even though only part if it is being shown.

--- DBViewer.py
+++ DBViewer.py
@@ -7,9 +7,6 @@ import streamlit as st
 st. set_page_config(layout="wide")
 
 import pandas as pd
-import numpy as np
-
-from streamlit_extras.function_explorer import function_explorer
 
 #Server connection info
 server   = 'pumpflow.database.windows.net'
@@ -20,31 +17,40 @@ driver   = '{ODBC Driver 17 for SQL Server}'
 
 st.title('Pumpflow SQL Database Searcher')
 
+TableNum = 0
+
 with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
     with conn.cursor() as cursor:
 
-        def SQLSearch_JobAndTest():
+        def SQLSearch_JobAndTest(TableNum):
 
+            LowerTableLimit = (TableNum*100)
+            UpperTableLimit = ((TableNum*100)+100)
 
+            #SelectTable = st.selectbox("Select a table to display",["MainTable","JobInfo","AddTemperatures","AddAuxPressure","AddAuxFlow","Value_Description"])
             JobNumber = st.number_input('Job Number',value=0)
             TestNumber = st.number_input('Test Number',value=0)
-
-
-            TempList = cursor.execute("SELECT TOP 100 * FROM MainTable WHERE Job_Number = ? AND Test_Number = ?",JobNumber,TestNumber).fetchall()
-            st.table(pd.DataFrame.from_records(TempList,columns=["Time","Job_Number","Test_Number","Take_Point","NGH","Efficiency","Absorbed_Power","Flow_Rate","Suction_Pressure","Discharge_Pressure","V1_DEX","V1_DEY","V1_DEZ","V2_NDEX","V2_NDEY","V2_NDEZ","TT1","Out_Torque","RPM_Used","Current","Voltage","Power","Current_P1","Current_P2","Current_P3","Frequency","LT1","PT_Atm"]))
+            TempList = cursor.execute("SELECT * FROM MainTable WHERE Job_Number = ? AND Test_Number = ?",JobNumber,TestNumber).fetchall()
+            st.table(pd.DataFrame.from_records(TempList,columns=["Time","Job_Number","Test_Number","Take_Point","NGH","Efficiency","Absorbed_Power","Flow_Rate","Suction_Pressure","Discharge_Pressure","V1_DEX","V1_DEY","V1_DEZ","V2_NDEX","V2_NDEY","V2_NDEZ","TT1","Out_Torque","RPM_Used","Current","Voltage","Power","Current_P1","Current_P2","Current_P3","Frequency","LT1","PT_Atm"]).iloc[LowerTableLimit:UpperTableLimit])
 
 
 
-        def SQLSearch_Date():
+        def SQLSearch_Date(TableNum):
+
+            LowerTableLimit = (TableNum*100)
+            UpperTableLimit = ((TableNum*100)+100)
+
+            #SelectTable = st.selectbox("Select a table to display",["MainTable","AddTemperatures","AddAuxPressure","AddAuxFlow"])
             Date = st.date_input("Choose a date")
-            TempList = cursor.execute("SELECT TOP 100 * FROM MainTable WHERE CONVERT(DATETIME, FLOOR(CONVERT(FLOAT, Time))) = ?",Date).fetchall()
-            st.table(pd.DataFrame.from_records(TempList,columns=["Time","Job_Number","Test_Number","Take_Point","NGH","Efficiency","Absorbed_Power","Flow_Rate","Suction_Pressure","Discharge_Pressure","V1_DEX","V1_DEY","V1_DEZ","V2_NDEX","V2_NDEY","V2_NDEZ","TT1","Out_Torque","RPM_Used","Current","Voltage","Power","Current_P1","Current_P2","Current_P3","Frequency","LT1","PT_Atm"]))
+            TempList = cursor.execute("SELECT * FROM MainTable WHERE CONVERT(DATETIME, FLOOR(CONVERT(FLOAT, Time))) = ?", Date).fetchall()
+            st.table(pd.DataFrame.from_records(TempList,columns=["Time","Job_Number","Test_Number","Take_Point","NGH","Efficiency","Absorbed_Power","Flow_Rate","Suction_Pressure","Discharge_Pressure","V1_DEX","V1_DEY","V1_DEZ","V2_NDEX","V2_NDEY","V2_NDEZ","TT1","Out_Torque","RPM_Used","Current","Voltage","Power","Current_P1","Current_P2","Current_P3","Frequency","LT1","PT_Atm"]).iloc[LowerTableLimit:UpperTableLimit])
 
 
-
-        SortMethod = st.radio('Sort by', ['Job and Test Number', 'Date'], horizontal=True)
+        
+        SortMethod = st.radio('Sort by', ['Date','Job and Test Number'], horizontal=True)
+        TableNum = st.number_input('Table Page',value=0)
 
         if SortMethod == 'Job and Test Number':
-            SQLSearch_JobAndTest()
+            SQLSearch_JobAndTest(TableNum)
         elif SortMethod == "Date":
-            SQLSearch_Date()
+            SQLSearch_Date(TableNum)
